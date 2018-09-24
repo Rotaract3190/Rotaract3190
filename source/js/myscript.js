089c5fcc15038d3a580af8888271700c6ec6a2df
Arun Teja Godavarthi <www.aruntejathefriend@gmail.com>
2018-09-25 01:21:00 +0530

    rss update (#45)

--- source/js/myscript.js
+++ source/js/myscript.js
@@ -88,14 +88,15 @@ $(document).ready(function() {
 			console.log(result.error);
 		} else {
 			var entries = result.feed.entries;
+			var blogsectiontext = "";
 				for(var i = 0; i < entries.length; i++){
 					var entry = entries[i];
+					blogsectiontext += '<div class="col-lg-4 col-md-6 col-xs-12"><div class="topproject-item"><div class="descr"><hr><h3 class="title"><a href="'+entry.link+'" target="_blank">'+entry.title+'</a></h3><a href="'+entry.link+'" target="_blank">Read More &Sc;</a></div></div></div>';
 
-					document.getElementById("blogsection").innerHTML = '<div class="col-lg-4 col-md-6 col-xs-12"><div class="topproject-item"><div class="descr"><hr><h3 class="title"><a href="'+entry.link+'" target="_blank">'+entry.title+'</a></h3><a href="'+entry.link+'" target="_blank">Read More &Sc;</a></div></div></div>';
-					
 					console.log(entry.title);
 					console.dir(entry);
 				}
+				document.getElementById("blogsection").innerHTML = blogsectiontext;
 		}
 	});            
 });
