b2a74d232c45712772e78b29da0ad89b0a7b6d05
Arun Teja Godavarthi <www.aruntejathefriend@gmail.com>
2018-10-24 00:22:31 +0530

    registration form for schools

--- rotafest2018/assets/js/app.js
+++ rotafest2018/assets/js/app.js
@@ -1,4 +1,4 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxUqksmmn9COqpy_GTXZMc0N9WwOW8kzdZY23ynwIiTJ75WawY/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbx02_qYPUm7HcZp3E60fn75R6_LKGD9Th8qvcji/exec'
 const form = document.forms['rotafest2018_3190gottalentregistration']
 form.addEventListener('submit', e => {
 	e.preventDefault()
@@ -7,7 +7,7 @@ form.addEventListener('submit', e => {
 	.catch(error => console.error('Error!', error.message))
 	
 	var x = document.getElementById("form-messages").value;
-	document.getElementById("form-messages").innerHTML = "Thanks for your interest in 3190 Got Talent, You will receive the link for payment shortly";
+	document.getElementById("form-messages").innerHTML = "Thanks for registration, we will contact you shortly.";
 	// Show alert
 	setTimeout(function(){
 		document.querySelector('.form-messages').style.display = 'block';
